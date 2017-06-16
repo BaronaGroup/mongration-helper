abd98f0cb894403048ed6289b0003c52b6c0893c
Tapani Haka <tapani.haka@barona.fi>
2017-06-16 17:16:53 +0300

    Use gulp for building

--- server/configure-node.js
+++ server/configure-node.js
@@ -1,5 +1,7 @@
-require('babel-register')
-require('babel-polyfill')
+if (!/build^/.test(__dirname)) {
+  require('babel-register')
+  require('babel-polyfill')
+}
 const P = require('bluebird')
 P.promisifyAll(require('request'))
 P.promisifyAll(require('express-session'))
